78d66b0a4d3944ad50b8fdf184127d49c73dc615
Bailey Capuano <bcapuano@asu.edu>
2022-12-22 12:10:01 -0500

    fix

--- mayhem/fuzz_uefi.py
+++ mayhem/fuzz_uefi.py
@@ -7,12 +7,29 @@ import fuzz_helpers
 with atheris.instrument_imports(include=["uefi_firmware"]):
     import uefi_firmware
 
+@atheris.instrument_func
 def TestOneInput(data):
+    if len(data) < 20:
+        return -1
     fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
     try:
-        uefi_firmware.AutoParser(fdp.ConsumeRemainingBytes())
-    except:
-        return -1
+        if fdp.ConsumeBool():
+            parser = uefi_firmware.AutoParser(fdp.ConsumeRemainingBytes())
+            if parser.type() != 'unknown':
+                firmware = uefi_firmware.parse()
+                firmware.showinfo()
+        else:
+            buff = fdp.ConsumeRemainingBytes()
+            comp_buff = uefi_firmware.efi_compressor.LzmaDecompress(
+                    buff, len(buff))
+            decomp_buff = uefi_firmware.efi_compressor.LzmaDecompress(
+                    comp_buff, len(comp_buff))
+            if len(decomp_buff) != len(buff) or buff != decomp_buff:
+                raise RuntimeError("Decompression failed!")
+    except Exception as e:
+        if 'decompress' in str(e):
+            return -1
+        raise
 
 
 def main():
