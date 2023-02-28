8d74380d9b0f94495a56ccec49f74e1af89836c0
xansec <76011430+xansec@users.noreply.github.com>
2023-02-28 15:45:11 -0500

    Merge pull request #1 from ennamarie19/mayhem
    Mayhem Heroes Integration

---
+++ mayhem/fuzz_uefi.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import atheris
+import sys
+import fuzz_helpers
+
+with atheris.instrument_imports(include=["uefi_firmware"]):
+    import uefi_firmware
+
+@atheris.instrument_func
+def TestOneInput(data):
+    if len(data) < 20:
+        return -1
+    fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
+    try:
+        if fdp.ConsumeBool():
+            parser = uefi_firmware.AutoParser(fdp.ConsumeRemainingBytes())
+            if parser.type() != 'unknown':
+                parser.parse()
+        else:
+            buff = fdp.ConsumeRemainingBytes()
+            comp_buff = uefi_firmware.efi_compressor.LzmaDecompress(
+                    buff, len(buff))
+            decomp_buff = uefi_firmware.efi_compressor.LzmaDecompress(
+                    comp_buff, len(comp_buff))
+    except Exception as e:
+        if 'decompress' in str(e):
+            return -1
+        raise
+
+
+def main():
+    atheris.Setup(sys.argv, TestOneInput)
+    atheris.Fuzz()
+
+
+if __name__ == "__main__":
+    main()
